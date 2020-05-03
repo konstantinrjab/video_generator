87684eeafc6deef88a5e1313addd397afb3a8636
konstantinrjab <konstantinrjab>
2020-05-03 21:13:37 +0300

    updated requirements.txt

--- app/app.py
+++ app/app.py
@@ -3,16 +3,24 @@ from random import randrange
 import random
 
 VIDEO_SIZE = (640, 480)
-DURATION = 5
-TREE_RANGE_HEIGHT = (220, 320)
+DURATION = 15
+TREE_RANGE_HEIGHT = (250, 320)
 TREE_DENSITY = 4
+TRAIN_SPEED = 25
 
 WHITE = (255, 255, 255)
 
-background_path = './assets/background.png'
-background = mpy.ImageClip(background_path). \
-    set_position(('center', 0)). \
-    resize(width=VIDEO_SIZE[0])
+
+def get_background():
+    return mpy.ImageClip('./assets/background.png') \
+        .set_position(('center', 0)) \
+        .resize(width=VIDEO_SIZE[0])
+
+
+def get_ground():
+    return mpy.ImageClip('./assets/ground.jpg', transparent=True) \
+        .set_position((0, -30)) \
+        .resize(width=VIDEO_SIZE[0])
 
 
 def get_trees(count):
@@ -28,7 +36,7 @@ def get_trees(count):
             'lambda t: (200 * t - x, y)',
             {
                 'y': randrange(TREE_RANGE_HEIGHT[0], TREE_RANGE_HEIGHT[1]),
-                'x': (tree_number * 25) + randrange(-VIDEO_SIZE[0], VIDEO_SIZE[0])
+                'x': (tree_number * TRAIN_SPEED) + randrange(-VIDEO_SIZE[0], VIDEO_SIZE[0])
             }
         )
         for tree_number in range(count)
@@ -42,13 +50,8 @@ def get_trees(count):
     return trees
 
 
-ground_path = './assets/ground.jpg'
-ground = mpy.ImageClip(ground_path, transparent=True). \
-    set_position((0, -30)). \
-    resize(width=VIDEO_SIZE[0])
-
 video = mpy.CompositeVideoClip(
-    [ground] + get_trees(DURATION * TREE_DENSITY) + [background],
+    [get_ground()] + get_trees(DURATION * TREE_DENSITY) + [get_background()],
     size=VIDEO_SIZE). \
     on_color(
     color=WHITE,

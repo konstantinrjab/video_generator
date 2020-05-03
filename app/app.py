5c8d21cb45f95fe626292b452a6edd6f33417e41
konstantinrjab <konstantinrjab>
2020-05-03 15:53:01 +0300

    add gizeh image on video

--- app/app.py
+++ app/app.py
@@ -1,34 +1,30 @@
 import moviepy.editor as mpy
-import gizeh as gz
-from math import pi
+import gizeh
 
-BLUE = (59 / 255, 89 / 255, 152 / 255)
 WHITE = (255, 255, 255)
 VIDEO_SIZE = (640, 480)
-
-
-def draw_stars(t):
-    surface = gz.Surface(640, 120)
-
-    for i in range(5):
-        star = gz.star(nbranches=5, radius=120 * 0.2,
-                       xy=[100 * (i + 1), 50], fill=(0, 1, 0),
-                       angle=t * pi)
-        star.draw(surface)
-    return surface.get_npimage()
-
 
 background_path = './assets/background2.png'
 background = mpy.ImageClip(background_path). \
     set_position(('center', 0)). \
     resize(width=VIDEO_SIZE[0])
 
-stars = mpy.VideoClip(draw_stars, duration=5)
+
+def make_frame(t):
+    surface = gizeh.Surface(width=VIDEO_SIZE[0], height=VIDEO_SIZE[1])
+    line = gizeh.polyline(points=[(0, 100), (200, 300)], stroke_width=10, stroke=(1, 0, 0))
+    line.draw(surface)
+    return surface.get_npimage(transparent=True)
+
+
+graphics_clip_mask = mpy.VideoClip(lambda t: make_frame(t)[:, :, 3] / 255.0, duration=5, ismask=True)
+graphics_clip = mpy.VideoClip(lambda t: make_frame(t)[:, :, :3],
+                              duration=5).set_mask(graphics_clip_mask)
 
 video = mpy.CompositeVideoClip(
     [
         background,
-        stars.set_position(
+        graphics_clip.set_position(
             ('center', 100)
         )
     ],

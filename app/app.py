c77b521eb26bf547232427d34e3e1fbdf15ce5f6
k.riabchenko <k.riabchenko@gamingtec.com>
2020-10-27 10:40:41 +0200

    layers position

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ import random
 import datetime
 import math
 
+VIDEO_SIZE = (480, 320)
+TRAIN_SPEED = 200
+
 
 class InfVideoClip(CompositeVideoClip):
     tree_paths = [
@@ -13,11 +16,10 @@ class InfVideoClip(CompositeVideoClip):
         './assets/tree3.png',
     ]
     processed_second = 0
-    clips_init_count = None
+    clips_on_top_layer_count = 1
+    clips_on_bottom_layer_count = 1
 
-    def get_frame(self, t):
-        if self.clips_init_count is None:
-            self.clips_init_count = len(self.clips)
+    def get_frame(self, t: float):
         current_second = math.floor(t)
 
         if self.processed_second != current_second:
@@ -26,20 +28,10 @@ class InfVideoClip(CompositeVideoClip):
             tree = mpy.ImageClip(random.choice(self.tree_paths), transparent=True). \
                 set_position(lambda t: (t * TRAIN_SPEED - current_second * TRAIN_SPEED, y)). \
                 resize(width=randrange(30, 50))
-            self.clips.append(tree)
+            self.clips.insert(len(self.clips) - self.clips_on_top_layer_count, tree)
             if len(self.clips) > 10:
-                self.clips.pop(self.clips_init_count)
-                # del self.clips[-1]
+                del self.clips[self.clips_on_bottom_layer_count]
         return super().get_frame(t)
-
-
-VIDEO_SIZE = (480, 320)
-TREE_Y_RANGE = (250, 320)
-TREE_DENSITY = 100
-TREE_DISTANCE_BETWEEN = 50
-TRAIN_SPEED = 200
-
-WHITE = (255, 255, 255)
 
 
 def get_background():
@@ -53,22 +45,19 @@ def get_ground():
         .set_position((0, -30)) \
         .resize(width=VIDEO_SIZE[0])
 
-video = InfVideoClip(
-    [get_ground()] + [get_background()],
-    # [get_ground()],
-    size=VIDEO_SIZE
-    ). \
-    on_color(color=WHITE, col_opacity=1)
+
+video = InfVideoClip([get_ground()] + [get_background()], size=VIDEO_SIZE). \
+    on_color(color=(255, 255, 255), col_opacity=1)
+
 
 time = 0
-duration = 20
+duration = 8
 folder = 'video/'
 counter = 0
 while counter < 1:
     subclip = video.subclip(time, time + duration)
 
-    now = datetime.datetime.now()
-    filename = str(now.hour) + str(now.minute) + str(now.second) + '.mp4'
+    filename = datetime.datetime.now().strftime('%H-%M-%S') + '.mp4'
     subclip.write_videofile(folder + filename, fps=25)
     time += duration
     counter += 1

9c43701880bf7b8289ec8c1e392a36ad3bcf350d
k.riabchenko <k.riabchenko@gamingtec.com>
2020-10-27 11:32:55 +0200

    fine naming & cleanup

--- app/app.py
+++ app/app.py
@@ -2,11 +2,28 @@ import moviepy.editor as mpy
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from random import randrange
 import random
-import datetime
 import math
+import time
+import os
 
 VIDEO_SIZE = (480, 320)
 TRAIN_SPEED = 200
+
+
+def get_background():
+    return mpy.ImageClip('./assets/background.png') \
+        .set_position(('center', 0)) \
+        .resize(width=VIDEO_SIZE[0])
+
+
+def get_ground():
+    return mpy.ImageClip('./assets/ground.jpg', transparent=True) \
+        .set_position((0, -30)) \
+        .resize(width=VIDEO_SIZE[0])
+
+
+def get_filename(video_current_time: int):
+    return 'video/' + str(video_current_time) + '.mp4'
 
 
 class InfVideoClip(CompositeVideoClip):
@@ -34,30 +51,27 @@ class InfVideoClip(CompositeVideoClip):
         return super().get_frame(t)
 
 
-def get_background():
-    return mpy.ImageClip('./assets/background.png') \
-        .set_position(('center', 0)) \
-        .resize(width=VIDEO_SIZE[0])
-
-
-def get_ground():
-    return mpy.ImageClip('./assets/ground.jpg', transparent=True) \
-        .set_position((0, -30)) \
-        .resize(width=VIDEO_SIZE[0])
-
-
 video = InfVideoClip([get_ground()] + [get_background()], size=VIDEO_SIZE). \
     on_color(color=(255, 255, 255), col_opacity=1)
 
+video_current_time = 0
+duration = 5
+clips = []
+video_end_time = time.time()
+buffer_time = 15
 
-time = 0
-duration = 8
-folder = 'video/'
-counter = 0
-while counter < 1:
-    subclip = video.subclip(time, time + duration)
+while True:
+    if time.time() >= video_end_time - buffer_time:
+        subclip = video.subclip(video_current_time, video_current_time + duration)
+        filename = get_filename(video_current_time)
+        subclip.write_videofile(filename, fps=25)
+        clips.append(filename)
 
-    filename = datetime.datetime.now().strftime('%H-%M-%S') + '.mp4'
-    subclip.write_videofile(folder + filename, fps=25)
-    time += duration
-    counter += 1
+        video_current_time += duration
+        video_end_time += duration
+        if len(clips) > buffer_time / duration:
+            os.remove(clips[0])
+            del clips[0]
+    else:
+        print('sleep...')
+        time.sleep(0.5)
